2d09f88112c7cbde0f3b1b09c0e26dcd3c6191c8
Zabandosk <orhun.ugur93@gmail.com>
2024-12-02 08:31:10 +0300

    temp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,11 +20,11 @@ Welcome, User!
 Thank you for visiting my site. 
 <br />
 I see this space as a small shop (dükkan) in the vast village of the internet, surrounded by large shopping malls run by big companies. This is a cosy corner where you can sit, have some tea or coffee, relax, and look around. It’s a piece of heritage I want to build with my own hands and hopefully preserve for the future, even though I’m not sure where it will lead.
-<br />
-I created this site for various reasons. 
+
+I wanted a space like this for several meaningful reasons.
 </p>
 <p>
-This is a space where I can share my things and thoughts online with the visitors who are willing to listen. No need to be seen in your favourite social media feed every time you refresh, buried in the vast sea of information, and no need for any algorithm to push me in front of you. I’m also a bit shy when it comes to social media. Instead of sharing there, I have this space to publish my reflections. <a href='#/clog'>Captain's Log</a> is where I plan to do this.
+This is a place where I can share my things and thoughts online with the visitors who are willing to listen. No need to be seen in your favourite social media feed every time you refresh, buried in the vast sea of information, and no need for any algorithm to push me in front of you. I’m also a bit shy when it comes to social media. Instead of sharing there, I have this space to publish my reflections. <a href='#/clog'>Captain's Log</a> is where I plan to do this.
 </p>
 <p>
 I want to use this site as a testing platform. I’m a self-taught developer, which means I occasionally break things as I learn. Naturally, I need an online space to test what I’ve been working on locally. So, you might encounter bugs from time to time! When that happens, I’ll remind myself of what Guide says: 'Don’t Panic'. And if you happen to see something glitch, feel free to tell yourself the same. And please <a href="mailto:orhun.ugur93@gmail.com">let me know</a> so I can fix it!
